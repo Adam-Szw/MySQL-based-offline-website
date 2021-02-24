4688e76ea18ef184b0ca6f0256e834008c1cc36f
O Gamble <og48@students.le.ac.uk>
2021-02-24 13:13:13 +0000

    BinaryBlitz updates

--- Games/BinaryBlitz/BinaryBlitz.js
+++ Games/BinaryBlitz/BinaryBlitz.js
@@ -5,6 +5,7 @@ function HideShowEasy() {
     document.getElementById("EasyGameDiv").style.display = "Block"
     EasyNumber();
     UpdateClock();
+    document.getElementById("AnswerLabelEasy").textContent = "";
 }
 
 function HideShowMedium() {
@@ -12,6 +13,7 @@ function HideShowMedium() {
     document.getElementById("MediumGameDiv").style.display = "Block"
     MediumNumber();
     UpdateClock();
+    document.getElementById("AnswerLabelMedium").textContent = "";
 }
 
 function HideShowHard() {
@@ -19,18 +21,30 @@ function HideShowHard() {
     document.getElementById("HardGameDiv").style.display = "Block"
     HardNumber();
     UpdateClock();
+    document.getElementById("AnswerLabelHard").textContent = "";
+}
+
+function HideShowCustom() {
+    document.getElementById("PregameContent").style.display = "None";
+    document.getElementById("CustomGameDiv").style.display = "Block"
+    UpdateClock();
 }
 
 function ResetPage() {
-    document.getElementById("HardGameDiv").style.display = "None"
-    document.getElementById("MediumGameDiv").style.display = "None"
-    document.getElementById("EasyGameDiv").style.display = "None"
+    document.getElementById("HardGameDiv").style.display = "None";
+    document.getElementById("MediumGameDiv").style.display = "None";
+    document.getElementById("EasyGameDiv").style.display = "None";
+    document.getElementById("CustomGameDiv").style.display = "None";
+    document.getElementById("EndGameDiv").style.display = "None";
     document.getElementById("PregameContent").style.display = "Block";
     UserScore =0;
     document.getElementById("UserScore").textContent = UserScore;
     clearTimeout(x);
     clock = 180;
     document.getElementById("GameTimer").textContent = clock;
+    document.getElementById("AnswerLabelEasy").textContent = "";
+    document.getElementById("AnswerLabelMedium").textContent = "";
+    document.getElementById("AnswerLabelHard").textContent = "";
 }
 
 function ShowHelpEasy() {
@@ -116,9 +130,11 @@ function CheckNumberEasy() {
                 document.getElementById("SubmissionEasy").value ="";
                 UserScore = UserScore + 1;
                 document.getElementById("UserScore").textContent = UserScore;
+                document.getElementById("AnswerLabelEasy").textContent = "Correct Answer!";
             }
             else { 
                 document.getElementById("SubmissionEasy").value ="";
+                document.getElementById("AnswerLabelEasy").textContent = "Incorrect Answer!";
             }
     }
     else {
@@ -137,9 +153,11 @@ function CheckNumberMedium() {
                 document.getElementById("SubmissionMedium").value ="";
                 UserScore = UserScore + 3;
                 document.getElementById("UserScore").textContent = UserScore;
+                document.getElementById("AnswerLabelMedium").textContent = "Correct Answer!";
             }
             else { 
                 document.getElementById("SubmissionMedium").value ="";
+                document.getElementById("AnswerLabelMedium").textContent = "Incorrect Answer!";
             }
     }
     else {
@@ -158,10 +176,11 @@ function CheckNumberHard() {
                 document.getElementById("SubmissionHard").value ="";
                 UserScore = UserScore+10;
                 document.getElementById("UserScore").textContent = UserScore;
-
+                document.getElementById("AnswerLabelHard").textContent = "Correct Answer!";
             }
             else { 
                 document.getElementById("SubmissionHard").value ="";
+                document.getElementById("AnswerLabelHard").textContent = "Incorrect Answer!";
             }
     }
     else {
@@ -178,18 +197,56 @@ function ConvertToDecimal(value) {
     return count;
 }
 
-var clock = 180
+var clock = 180;
 var timeoutvariable = 100;
 
 function UpdateClock() { 
 	clock = Math.round(clock*10-1)/10;
 	document.getElementById("GameTimer").textContent = clock;
 	if(clock > 0)
-	{
+	{   
+        if(clock > 120) {
+            document.getElementById("TimeLeftEasy").textContent = "3 Minutes Left!";
+            document.getElementById("TimeLeftEasy").style.color = "Black";
+            document.getElementById("TimeLeftMedium").textContent = "3 Minutes Left!";
+            document.getElementById("TimeLeftMedium").style.color = "Black";
+            document.getElementById("TimeLeftHard").textContent = "3 Minutes Left!";
+            document.getElementById("TimeLeftHard").style.color = "Black";
+        }
+        else if (clock > 60) {
+            document.getElementById("TimeLeftEasy").textContent = "2 Minutes Left!";
+            document.getElementById("TimeLeftEasy").style.color = "#c3c907";
+            document.getElementById("TimeLeftMedium").textContent = "2 Minutes Left!";
+            document.getElementById("TimeLeftMedium").style.color = "#c3c907"
+            document.getElementById("TimeLeftHard").textContent = "2 Minutes Left!";
+            document.getElementById("TimeLeftHard").style.color = "#c3c907"
+        }
+        else if (clock > 30) {
+            document.getElementById("TimeLeftEasy").textContent = "1 Minute Left!";
+            document.getElementById("TimeLeftEasy").style.color = "#eea526";
+            document.getElementById("TimeLeftMedium").textContent = "1 Minute Left!";
+            document.getElementById("TimeLeftMedium").style.color = "#eea526";
+            document.getElementById("TimeLeftHard").textContent = "1 Minute Left!";
+            document.getElementById("TimeLeftHard").style.color = "#eea526";
+        }
+        else if (clock <= 30) {
+            document.getElementById("TimeLeftEasy").textContent = "30 Seconds Left!";
+            document.getElementById("TimeLeftEasy").style.color="#f03d31"
+            document.getElementById("TimeLeftMedium").textContent = "30 Seconds Left!";
+            document.getElementById("TimeLeftMedium").style.color="#f03d31"
+            document.getElementById("TimeLeftHard").textContent = "30 Seconds Left!";
+            document.getElementById("TimeLeftHard").style.color="#f03d31"
+        }
 		x = setTimeout(UpdateClock, timeoutvariable);
 	}
 	else
 	{
-		alert("Game over")
+        document.getElementById("EasyGameDiv").style.display = "None"
+        document.getElementById("MediumGameDiv").style.display = "None"
+        document.getElementById("HardGameDiv").style.display = "None"
+        document.getElementById("CustomGameDiv").style.display = "None"
+        document.getElementById("EndGameDiv").style.display = "Block"
+		alert("Game over");
 	}
 }
+
