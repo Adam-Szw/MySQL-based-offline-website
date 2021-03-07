5c0a8c7e37a4f90259c639b3cf8beb42c87daec6
O Gamble <og48@students.le.ac.uk>
2021-03-07 18:37:48 +0000

    BinaryBlitz updates & Signin updates

--- Games/BinaryBlitz/BinaryBlitz.js
+++ Games/BinaryBlitz/BinaryBlitz.js
@@ -1,40 +1,28 @@
 var UserScore = 0;
 
-function HideShowEasy() { 
+/*This is the function to start the game and hide the initial view*/
+function HideShow(Number, Value, Section, Label) {
     document.getElementById("PregameContent").style.display = "None";
-    document.getElementById("EasyGameDiv").style.display = "Block"
-    EasyNumber();
+    document.getElementById(Section).style.display = "Block"
+    GenerateNumber(Number, Value);
     UpdateClock();
-    document.getElementById("AnswerLabelEasy").textContent = "";
+    document.getElementById(Label).textContent = "";
 }
 
-function HideShowMedium() {
-    document.getElementById("PregameContent").style.display = "None";
-    document.getElementById("MediumGameDiv").style.display = "Block"
-    MediumNumber();
-    UpdateClock();
-    document.getElementById("AnswerLabelMedium").textContent = "";
-}
-
-function HideShowHard() {
-    document.getElementById("PregameContent").style.display = "None";
-    document.getElementById("HardGameDiv").style.display = "Block"
-    HardNumber();
-    UpdateClock();
-    document.getElementById("AnswerLabelHard").textContent = "";
-}
-
+/*Hide and show the custom game area*/
 function HideShowCustom() {
     document.getElementById("PregameContent").style.display = "None";
     document.getElementById("CustomGameDiv").style.display = "Block"
     UpdateClock();
 }
 
+/*Hide and show the lecturer game maker*/
 function HideShowLecturerOption() {
     document.getElementById("PregameContent").style.display = "None";
     document.getElementById("LecturerGameDiv").style.display = "Block"
 }
 
+/*This section resets the page when the user exits it*/
 function ResetPage() {
     document.getElementById("HardGameDiv").style.display = "None";
     document.getElementById("MediumGameDiv").style.display = "None";
@@ -53,37 +41,21 @@ function ResetPage() {
     document.getElementById("AnswerLabelHard").textContent = "";
 }
 
-function ShowHelpEasy() {
-    if (document.getElementById("HelpContentEasy").style.display == "none") {
-        document.getElementById("HelpContentEasy").style.display = "Block";
+/*Each of the practice modes have a help area, this is to hide it or show it*/
+function ShowHelp(ItemName) {
+    if (document.getElementById(ItemName).style.display == "none") {
+        document.getElementById(ItemName).style.display = "Block";
     }
     else {
-        document.getElementById("HelpContentEasy").style.display = "none";
+        document.getElementById(ItemName).style.display = "none";
     }
 }
 
-function ShowHelpMedium() {
-    if (document.getElementById("HelpContentMedium").style.display == "none") {
-        document.getElementById("HelpContentMedium").style.display = "Block";
-    }
-    else {
-        document.getElementById("HelpContentMedium").style.display = "none";
-    }
-}
-
-function ShowHelpHard() {
-    if (document.getElementById("HelpContentHard").style.display == "none") {
-        document.getElementById("HelpContentHard").style.display = "Block";
-    }
-    else {
-        document.getElementById("HelpContentHard").style.display = "none";
-    }
-}
-
-function EasyNumber() {
+/*This function generates a random binary number*/
+function GenerateNumber(ItemName, NumberLength) {
     var i;
     var text = "";
-    for (i = 0; i < 4; i++) {
+    for (i = 0; i < NumberLength; i++) {
         var x = Math.round(Math.random())
         if (x==1) {
             text = text + "1";
@@ -92,55 +64,26 @@ function EasyNumber() {
             text = text + "0";
         }
     }
-    document.getElementById("EasyNumber").textContent = text;
+    document.getElementById(ItemName).textContent = text;
 }
 
-function MediumNumber() {
-    var i;
-    var text = "";
-    for (i = 0; i < 8; i++) {
-        var x = Math.round(Math.random())
-        if (x==1) {
-            text = text + "1";
-        }
-        else {
-            text = text + "0";
-        }
-    }
-    document.getElementById("MediumNumber").textContent = text;
-}
-
-function HardNumber() {
-    var i;
-    var text = "";
-    for (i = 0; i < 12; i++) {
-        var x = Math.round(Math.random())
-        if (x==1) {
-            text = text + "1";
-        }
-        else {
-            text = text + "0";
-        }
-    }
-    document.getElementById("HardNumber").textContent = text;
-}
-
-function CheckNumberEasy() {
-    var value = document.getElementById("EasyNumber").textContent;
-    var user = document.getElementById("SubmissionEasy").value;
+/*This section is used to check whether the user input is correcct*/
+function CheckNumber(Number, Submission, Label, Value, Score) {
+    var value = document.getElementById(Number).textContent;
+    var user = document.getElementById(Submission).value;
     value = value.split('');
     if (user != "") {
         cputotal = ConvertToDecimal(value);
             if (cputotal == parseInt(user)) {
-                EasyNumber();
-                document.getElementById("SubmissionEasy").value ="";
-                UserScore = UserScore + 1;
+                GenerateNumber(Number,Value);
+                document.getElementById(Submission).value ="";
+                UserScore = UserScore + Score;
                 document.getElementById("UserScore").textContent = UserScore;
-                document.getElementById("AnswerLabelEasy").textContent = "Correct Answer!";
+                document.getElementById(Label).textContent = "Correct Answer!";
             }
             else { 
-                document.getElementById("SubmissionEasy").value ="";
-                document.getElementById("AnswerLabelEasy").textContent = "Incorrect Answer!";
+                document.getElementById(Submission).value ="";
+                document.getElementById(Label).textContent = "Incorrect Answer!";
             }
     }
     else {
@@ -148,52 +91,7 @@ function CheckNumberEasy() {
     }
 }
 
-function CheckNumberMedium() {
-    var value = document.getElementById("MediumNumber").textContent;
-    var user = document.getElementById("SubmissionMedium").value;
-    value = value.split('');
-    if (user != "") {
-        cputotal = ConvertToDecimal(value);
-            if (cputotal == parseInt(user)) {
-                MediumNumber();
-                document.getElementById("SubmissionMedium").value ="";
-                UserScore = UserScore + 3;
-                document.getElementById("UserScore").textContent = UserScore;
-                document.getElementById("AnswerLabelMedium").textContent = "Correct Answer!";
-            }
-            else { 
-                document.getElementById("SubmissionMedium").value ="";
-                document.getElementById("AnswerLabelMedium").textContent = "Incorrect Answer!";
-            }
-    }
-    else {
-        alert("Enter your answer in the submission box")
-    }
-}
-
-function CheckNumberHard() {
-    var value = document.getElementById("HardNumber").textContent;
-    var user = document.getElementById("SubmissionHard").value;
-    value = value.split('');
-    if (user != "") {
-        cputotal = ConvertToDecimal(value);
-            if (cputotal == parseInt(user)) {
-                HardNumber();
-                document.getElementById("SubmissionHard").value ="";
-                UserScore = UserScore+10;
-                document.getElementById("UserScore").textContent = UserScore;
-                document.getElementById("AnswerLabelHard").textContent = "Correct Answer!";
-            }
-            else { 
-                document.getElementById("SubmissionHard").value ="";
-                document.getElementById("AnswerLabelHard").textContent = "Incorrect Answer!";
-            }
-    }
-    else {
-        alert("Enter your answer in the submission box")
-    }
-}
-
+/*This section is used to convert a number from binary to decimal*/
 function ConvertToDecimal(value) {
     var counter;
     var count=0;
@@ -206,6 +104,7 @@ function ConvertToDecimal(value) {
 var clock = 180;
 var timeoutvariable = 100;
 
+/*This section is used to run the clock when the user starts a game*/
 function UpdateClock() { 
 	clock = Math.round(clock*10-1)/10;
 	document.getElementById("GameTimer").textContent = clock;
@@ -266,11 +165,14 @@ function UpdateClock() {
 
 var counter =0;
 
+/*This section adds a question to the questionarea section*/
 function AddQuestion() {
     /*Show the number of questions here*/
     counter=counter+1;
     $("#QuestionArea").append('<table class="Question"><tr><td colspan="3">Question Number '+ counter +':</td></tr><tr><td>Type:</td><td>&nbsp;&nbsp;<select onchange="QuestionChange('+counter+')" class="Conversion"><option>Binary to Decimal</option><option>Decimal to Binary</option><option>Hexadecimal to Decimal</option><option>Decimal to Hexadecimal</option></select><br> </td></tr><tr><td>Input:</td><td><input class="Input"></td><td class="InputExample">&nbsp;&nbsp;Example: 101010</td></tr><tr><td>Timer:</td><td><input type="number" min="1" max="120" class="Timer"></td><td class="TimerExample">&nbsp;&nbsp;Example: 23 seconds</td></tr><tr><td>Points:</td><td><input type="number" min="1" max="100" class="Points"></td><td class="PointExample">&nbsp;&nbsp;Example: 5 points</td></tr><tr><td colspan="3"><button id="QuestionSave" onclick="SaveQuestion('+counter+')">Submit Button</button><button id="QuestionReset" onclick="ResetQuestion('+counter+')">Reset Button</button><button id="QuestionDelete" onclick="DeleteQuestion('+counter+')">Delete Last Button</button></td></tr></table>');
 }
+
+/*This section is used to save a question, each entry must be valid*/
 function SaveQuestion(ItemNumber) {
     var choice = document.getElementsByClassName("Conversion")[ItemNumber-1].value;
     var input = document.getElementsByClassName("Input")[ItemNumber-1].value;
@@ -321,6 +223,7 @@ function SaveQuestion(ItemNumber) {
     }
 }
 
+/*This section is used to change the question labels when the dropdown is changed*/
 function QuestionChange(ItemNumber) {
     var choice = document.getElementsByClassName("Conversion")[ItemNumber-1].value;
     if (choice == "Binary to Decimal") {
@@ -337,16 +240,17 @@ function QuestionChange(ItemNumber) {
     }
 }
 
+/*This section is called when the user presses the reset button*/
 function ResetQuestion(ItemNumber) {
     document.getElementsByClassName("Input")[ItemNumber-1].value = "";
     document.getElementsByClassName("Points")[ItemNumber-1].value = "";
     document.getElementsByClassName("Timer")[ItemNumber-1].value = "";
 }
 
+/**This section is used to delete the last question*/
 function DeleteQuestion(ItemNumber) {
     var Len = document.getElementsByClassName("Question").length
     var DeleteObject = document.getElementsByClassName("Question")[Len-1];
     DeleteObject.remove();
     counter = counter-1;
 }
-
