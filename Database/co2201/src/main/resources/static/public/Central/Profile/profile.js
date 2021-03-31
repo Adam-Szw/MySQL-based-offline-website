c0e36053f08eae2e4073201e8e4212458fdad23b
Jaynik Parsotomo <jp539@student.le.ac.uk>
2021-03-31 12:10:52 +0100

    Numbers only allowed in phone number input

--- Database/co2201/src/main/resources/static/public/Central/Profile/profile.js
+++ Database/co2201/src/main/resources/static/public/Central/Profile/profile.js
@@ -428,6 +428,27 @@ function saveUserData(loggedPlayerID){
 	
 }
 
+function setInputFilter(textbox, inputFilter) {
+  ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop"].forEach(function(event) {
+    textbox.addEventListener(event, function() {
+      if (inputFilter(this.value)) {
+        this.oldValue = this.value;
+        this.oldSelectionStart = this.selectionStart;
+        this.oldSelectionEnd = this.selectionEnd;
+      } else if (this.hasOwnProperty("oldValue")) {
+        this.value = this.oldValue;
+        this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+      } else {
+        this.value = "";
+      }
+    });
+  });
+}
+
+setInputFilter(document.getElementById("user-phone"), function(value) {
+  return /^\d*$/.test(value); });
+
+
 //Function to save user bio
 function saveUserBio(loggedPlayerID){
 	
